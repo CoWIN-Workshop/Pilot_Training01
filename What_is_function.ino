ca993037c88745525c81e8bbf4373c90673de50c
CoWIN-Workshop <90884001+CoWIN-Workshop@users.noreply.github.com>
2021-11-08 18:34:56 +0800

    Add files via upload

---
+++ What_is_function.ino
@@ -0,0 +1,14 @@
+int sum_func (int x, int y) // function declaration {
+   int z = 0;
+   z = x+y ;
+   return z; // return the value
+}
+
+void setup () {
+   Statements // group of statements
+}
+
+Void loop () {
+   int result = 0 ;
+   result = Sum_func (5,6) ; // function call
+}
